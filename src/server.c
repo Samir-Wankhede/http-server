27447092bc9b14203ba33af5aeb00becc4f08c7d
Samir Wankhede <samirsw04@gmail.com>
2026-03-06 00:53:32 +0530

    updated implementation to use pthreads

--- src/server.c
+++ src/server.c
@@ -1,6 +1,8 @@
 #include "httpserver/config.h"
 #include "log_handler/logger.h"
 #include "socket_handler/socket.h"
+#include "request_handlers/dispatcher.h"
+#include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,6 +10,14 @@
 #include <sys/socket.h>
 
 int main(void){
+    // set logger level from config
+    for(int i=LOG_INFO; i<=LOG_FATAL; i++){
+        if(strcmp(LOGGING_LEVEL, levels[i]) == 0){
+            log_set_level((log_level_types)i);
+            break;
+        }
+    }
+    
     int sockfd;
     if((sockfd = initialise_socket(DEFAULT_PORT)) == -1){
         exit(EXIT_FAILURE);
@@ -22,22 +32,20 @@ int main(void){
         if(client_sock_fd == -1){
             continue;
         }
-        if(!fork()){
-            close(sockfd);
-            char ip[INET6_ADDRSTRLEN];
-            char port[PORTSTRLEN];
-            if(get_peer_by_sockfd(client_sock_fd, ip, sizeof(ip), port, sizeof(port)) == -1){
-                LOG_ERROR("Could not get ip and port from peer");
-            }
-            LOG_INFO("Attempting to send status to %s:%s", ip, port);
-            char *response = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 42\r\nConnection: close\r\n\r\n<html><body><h1>Hello World!</body></html>";
-            if(send_response(client_sock_fd, response, strlen(response), 0) == -1){
-                LOG_ERROR("sending error: ");
-            }
-            close(client_sock_fd);
-            exit(EXIT_FAILURE);
+
+        pthread_t thread;
+        char ip[INET6_ADDRSTRLEN];
+        char port[PORTSTRLEN];
+        if(pthread_create(&thread, NULL, dispatch_request, (void *)(intptr_t)client_sock_fd) == 0){
+            if(get_peer_by_sockfd(client_sock_fd, ip, sizeof(ip), port, sizeof(port)) == 0)
+                LOG_DEBUG("Thread: %lu is handling client: %s:%s", thread, ip, port);
+            pthread_detach(thread);
         }
-        close(client_sock_fd);
+        else{
+            LOG_ERROR("Failed to create thread for client: %d", client_sock_fd);
+            close(client_sock_fd);
+        }
     }
+    close(sockfd);
     return 0;
 }
